65157eda4858598129c24079b9f905577fd3e5c4
Tim Regan <timregan@microsoft.com>
2021-04-20 11:51:03 +0100

    Returning GUID and tidy-up

--- submit_for_inference.py
+++ submit_for_inference.py
@@ -26,6 +26,7 @@ RUN_SCORING_SCRIPT = "download_model_and_run_scoring.py"
 # The property in the model registry that holds the name of the Python environment
 PYTHON_ENVIRONMENT_NAME = "python_environment_name"
 IMAGEDATA_FILE_NAME = "imagedata.zip"
+IMAGEDATA_PATH_PREFIX = "temp-image-store"
 
 @dataclass
 class SubmitForInferenceConfig:
@@ -44,28 +45,36 @@ def create_run_config(azure_config: AzureConfig,
     Creates a configuration to run the InnerEye training script in AzureML.
     :param azure_config: azure related configurations to use for model scale-out behaviour
     :param source_config: configurations for model execution, such as name and execution mode
-    :param environment_name: If specified, try to retrieve the existing Python environment with this name. If that
-    is not found, create one from the Conda files provided in `source_config`. This parameter is meant to be used
-    when running inference for an existing model.
+    :param environment_name: If specified, try to retrieve the existing Python environment with
+    this name. If that     is not found, create one from the Conda files provided in
+    `source_config`. This parameter is meant to be used when running inference for an existing
+    model.
     :return: The configured script run.
     """
-    # AzureML seems to sometimes expect the entry script path in Linux format, hence convert to posix path
-    entry_script_relative_path = source_config.entry_script.relative_to(source_config.root_folder).as_posix()
-    logging.info(f"Entry script {entry_script_relative_path} ({source_config.entry_script} relative to "
-                 f"source directory {source_config.root_folder})")
+    # AzureML seems to sometimes expect the entry script path in Linux format, hence convert to
+    # posix path
+    entry_script_relative_path =\
+        source_config.entry_script.relative_to(source_config.root_folder).as_posix()
+    logging.info(f"Entry script {entry_script_relative_path} ({source_config.entry_script} "
+                 f"relative to source directory {source_config.root_folder})")
     max_run_duration = None
     workspace = azure_config.get_workspace()
     run_config = RunConfiguration(
         script=entry_script_relative_path,
         arguments=source_config.script_params,
     )
-    env = Environment.get(azure_config.get_workspace(), name=environment_name, version=ENVIRONMENT_VERSION)
+    env = Environment.get(
+        azure_config.get_workspace(),
+        name=environment_name,
+        version=ENVIRONMENT_VERSION)
     logging.info(f"Using existing Python environment '{env.name}'.")
     run_config.environment = env
     run_config.target = azure_config.cluster
     run_config.max_run_duration_seconds = max_run_duration
-    # Use blob storage for storing the source, rather than the FileShares section of the storage account.
-    run_config.source_directory_data_store = workspace.datastores.get(WORKSPACE_DEFAULT_BLOB_STORE_NAME).name
+    # Use blob storage for storing the source, rather than the FileShares section of the storage
+    # account.
+    run_config.source_directory_data_store =\
+        workspace.datastores.get(WORKSPACE_DEFAULT_BLOB_STORE_NAME).name
     script_run_config = ScriptRunConfig(
         source_directory=str(source_config.root_folder),
         run_config=run_config,
@@ -73,13 +82,16 @@ def create_run_config(azure_config: AzureConfig,
     return script_run_config
 
 
-def submit_for_inference(args: SubmitForInferenceConfig, workspace: Workspace, azure_config: AzureConfig) -> str:
+def submit_for_inference(
+    args: SubmitForInferenceConfig,
+    workspace: Workspace,
+    azure_config: AzureConfig) -> str:
     """
     Create and submit an inference to AzureML, and optionally download the resulting segmentation.
     :param args: configuration, see SubmitForInferenceConfig
     :param workspace: Azure ML workspace.
     :param azure_config: An object with all necessary information for accessing Azure.
-    :return: Azure Run Id.
+    :return: Azure Run Id (and the target path of the image data zip for unit tests to check)
     """
     logging.info("Identifying model")
     model = Model(workspace=workspace, id=args.model_id)
@@ -96,22 +108,24 @@ def submit_for_inference(args: SubmitForInferenceConfig, workspace: Workspace, a
     image_path.write_bytes(args.image_data)
 
     default_datastore = workspace.get_default_datastore()
-    target_path = f"temp-image-store/{str(uuid.uuid4())}"
+    target_path = f"{IMAGEDATA_PATH_PREFIX}/{str(uuid.uuid4())}"
     default_datastore.upload_files(
         files=[str(image_path)],
         target_path=target_path,
         overwrite=False,
         show_progress=False)
     image_path.unlink()
-  
-    # Retrieve the name of the Python environment that the training run used. This environment should have been
-    # registered. If no such environment exists, it will be re-create from the Conda files provided.
+
+    # Retrieve the name of the Python environment that the training run used. This environment
+    # should have been registered. If no such environment exists, it will be re-create from the
+    # Conda files provided.
     python_environment_name = model.tags.get(PYTHON_ENVIRONMENT_NAME, "")
     if python_environment_name == "":
-        raise ValueError(f"Model ID: {model_id} does not contain an environment tag {PYTHON_ENVIRONMENT_NAME}")
+        raise ValueError(
+            f"Model ID: {model_id} does not contain an environment tag {PYTHON_ENVIRONMENT_NAME}")
 
-    # Copy the scoring script from the repository. This will start the model download from Azure, and invoke the
-    # scoring script.
+    # Copy the scoring script from the repository. This will start the model download from Azure,
+    # and invoke the scoring script.
     entry_script = source_directory_path / Path(RUN_SCORING_SCRIPT).name
     current_file_path = Path(os.path.dirname(os.path.realpath(__file__)))
     shutil.copyfile(current_file_path / str(RUN_SCORING_SCRIPT),
@@ -123,18 +137,21 @@ def submit_for_inference(args: SubmitForInferenceConfig, workspace: Workspace, a
                        "--model-id", model_id,
                        "--datastore-image-path", f"{target_path}",
                        SCORE_SCRIPT,
-                       # The data folder must be relative to the root folder of the AzureML job. test_image_files
-                       # is then just the file relative to the data_folder
+                       # The data folder must be relative to the root folder of the AzureML job.
+                       # test_image_files is then just the file relative to the data_folder
                        "--data_folder", image_path.parent.name,
                        "--image_files", image_path.name,
                        "--use_dicom", "True",
                        "--model_id", model_id],
     )
-    run_config = create_run_config(azure_config, source_config, environment_name=python_environment_name)
+    run_config = create_run_config(
+        azure_config,
+        source_config,
+        environment_name=python_environment_name)
     exp = Experiment(workspace=workspace, name=args.experiment_name)
     run = exp.submit(run_config)
     logging.info(f"Submitted run {run.id} in experiment {run.experiment.name}")
     logging.info(f"Run URL: {run.get_portal_url()}")
     source_directory.cleanup()
     logging.info(f"Deleted submission directory {source_directory_path}")
-    return run.id
+    return run.id, target_path
